b97f01ddd1a35af495567fd8276d83b7da80bc92
stephenwijayautama-netizen <stephenwijayautama@gmail.com>
2026-02-04 13:33:03 +0700

    hallo

---
+++ StudioKayu/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+// src/pages/Home.tsx
+const Home = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Selamat Datang di StudioKayu</h1>
+      <p>Halaman Home berhasil dimuat!</p>
+    </div>
+  );
+};
+
+export default Home;
