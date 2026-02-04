8566359abd2f880947cd786631851836629dd6fe
Rod1213456 <roddricksuanto@gmail.com>
2026-02-04 16:02:30 +0700

    initial commit

--- StudioKayu/src/App.tsx
+++ StudioKayu/src/App.tsx
@@ -1,6 +1,7 @@
 // App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'; // Ini file yang ada di screenshot kamu
+import Tambahin_keranjang from './pages/Tambahin_keranjang';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         {/* Setting utama: localhost:5173/ akan menampilkan Home.tsx */}
         <Route path="/" element={<Home />} />
+        <Route path="/Tambahin_keranjang" element={<Tambahin_keranjang />} />
+
       </Routes>
     </Router>
   );
