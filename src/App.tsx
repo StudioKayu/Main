cd61e35aa3bdd8cc5f732ec50da08c1ab9441d84
stephenwijayautama-netizen <stephenwijayautama@gmail.com>
2026-02-16 20:20:05 +0700

    update1

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+// App.tsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'; // Ini file yang ada di screenshot kamu
+import Tambahin_keranjang from './pages/Tambahin_keranjang';
+import Promo from './pages/Promo';
+import Product_Terlaris from './pages/Product_Terlaris';
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Setting utama: localhost:5173/ akan menampilkan Home.tsx */}
+        <Route path="/" element={<Home />} />
+        <Route path="/Tambahin_keranjang" element={<Tambahin_keranjang />} />
+        <Route path="/Promo" element={<Promo />} />
+        <Route path="/Product_Terlaris" element={<Product_Terlaris />} />
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
