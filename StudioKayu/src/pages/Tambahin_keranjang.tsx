5b3155d8e5f8055a2a6a9fa388a828a0ac0412f2
willysrh08 <serehwilly@gmail.com>
2026-02-24 14:06:35 +0700

    update keranjang

--- StudioKayu/src/pages/Tambahin_keranjang.tsx
+++ StudioKayu/src/pages/Tambahin_keranjang.tsx
@@ -1,11 +1,11 @@
 const Tambahin_keranjang = () => {
   return (
-    <div style={{ padding: '20px' }}>
+    <div style={{ padding: "20px" }}>
       <h1>Selamat Datang saya di StudioKayu</h1>
       <p>Halaman home berhasil dimuat!</p>
       <p>halllo</p>
+      <p>oke kali ini udah berhasil</p>
     </div>
-    
   );
 };
 
